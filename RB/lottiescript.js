f2313c12d046d1f7517ffbdbfab9a5635e0b9a37
keffolasse <lasse@b-reel.com>
2021-09-13 12:32:00 +0200

    Update lottiescript.js

--- RB/lottiescript.js
+++ RB/lottiescript.js
@@ -1,7 +1,7 @@
 import lottieWeb from 'https://cdn.skypack.dev/lottie-web';
 
 var animation = lottieWeb.loadAnimation({
-  container: document.getElementById('container'),
+  container: document.getElementById('demo'),
   path: 'https://maxst.icons8.com/vue-static/landings/animated-icons/icons/pause/pause.json',
   renderer: 'svg',
   loop: true,
