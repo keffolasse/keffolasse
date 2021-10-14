40e380b70ff66331220c47116c455b3c80477683
keffolasse <lasse@b-reel.com>
2021-10-14 15:18:10 +0200

    Update bar-graph.js

--- tag_tracker/js/bar-graph.js
+++ tag_tracker/js/bar-graph.js
@@ -3,7 +3,7 @@ let bars = {};
 let graphWrapper;
 
 // these are the colors of our bars
-let colors = ['#090040', '#8a00ff', '#00ff24', '#fff000', '#ed4d17'];
+let colors = ['#3623aa', '#8a00ff', '#00ff24', '#fff000', '#ed4d17'];
 let lightColors = ['#FFECE2', '#FFE9EC', '#F1F0FF', '#E2F5FF'];
 
 // This function makes the bar graph
